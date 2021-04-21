8747928813ebd01c1843010507fb8a305d5e6139
Tayyab Hussain03 <tayyabfaisal106@gmail.com>
2021-04-21 12:18:06 -0700

    ramadan calender complete

---
+++ app.js
@@ -0,0 +1,137 @@
+var input = +prompt("Enter Roza Here:");
+
+var roza = ["1st ROZA", "2nd ROZA", "3rd ROZA", "4th ROZA", "5th ROZA", "6th ROZA", "7th ROZA", "8th ROZA", "9th ROZA",
+    "10th ROZA", "11th ROZA", "12th ROZA", "13th ROZA", "14th ROZA", "15th ROZA", "16th ROZA", "17th ROZA", "18th ROZA",
+    "19th ROZA", "20th ROZA", "21th ROZA", "22th ROZA", "23th ROZA", "24th ROZA", "25th ROZA", "26th ROZA", "27th ROZA",
+    "28th ROZA", "29th ROZA", "30th ROZA"];
+
+var sehrTime = ["4:52 AM", "4:51 AM", "4:50 AM", "4:49 AM", "4:48 AM", "4:47 AM", "4:48 AM", "4:46 AM",
+    "4:45 AM", "4:44 AM", "4:43 AM", "4:42 AM", "4:41 AM", "4:40 AM", "4:39 AM", "4:38 AM", "4:38 AM", "4:37 AM",
+    "4:36 AM", "4:35 AM", "4:34 AM", "4:33 AM", "4:32 AM", "4:31 AM", "4:30 AM", "4:29 AM", "4:28 AM", "4:27 AM",
+    "4:26 AM", "4:25 AM", "4:24 AM"];
+
+var iftarTime = ["6:55 PM", "6:55 PM", "6:56 PM", "6:56 PM", "6:57 PM", "6:57 PM", "6:57 PM", "6:58 PM",
+    "6:58 PM", "6:59 PM", "6:59 PM", "7:00 PM", "7:00 PM", "7:01 PM", "7:01 PM", "7:02 PM", "7:02 PM", "7:03 PM",
+    "7:03 PM", "7:04 PM", "7:04 PM", "7:05 PM", "7:05 PM", "7:06 PM", "7:06 PM", "7:07 PM", "7:07 PM", "7:08 PM",
+    "7:08 PM", "7:09 PM"];
+
+if (input === 1) {
+    document.write("<h1>" + roza[0] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[0] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[0] + "<h3/>")
+}
+
+else if (input === 2) {
+    document.write("<h1>" + roza[1] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[1] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[1] + "<h3/>")
+}
+
+else if (input === 3) {
+    document.write("<h1>" + roza[2] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[2] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[2] + "<h3/>")
+}
+
+else if (input === 4) {
+    document.write("<h1>" + roza[3] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[3] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[3] + "<h3/>")
+}
+
+else if (input === 5) {
+    document.write("<h1>" + roza[4] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[4] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[4] + "<h3/>")
+}
+
+else if (input === 6) {
+    document.write("<h1>" + roza[5] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[5] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[5] + "<h3/>")
+}
+
+else if (input === 7) {
+    document.write("<h1>" + roza[6] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[6] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[6] + "<h3/>")
+}
+
+else if (input === 8) {
+    document.write("<h1>" + roza[7] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[17] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[7] + "<h3/>")
+}
+
+else if (input === 9) {
+    document.write("<h1>" + roza[8] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[8] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[8] + "<h3/>")
+}
+
+else if (input === 10) {
+    document.write("<h1>" + roza[9] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[9] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[9] + "<h3/>")
+}
+
+else if (input === 11) {
+    document.write("<h1>" + roza[10] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[10] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[10] + "<h3/>")
+}
+
+else if (input === 12) {
+    document.write("<h1>" + roza[11] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[11] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[11] + "<h3/>")
+}
+
+else if (input === 13) {
+    document.write("<h1>" + roza[12] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[12] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[12] + "<h3/>")
+}
+
+else if (input === 14) {
+    document.write("<h1>" + roza[13] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[13] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[13] + "<h3/>")
+}
+
+else if (input === 15) {
+    document.write("<h1>" + roza[14] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[14] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[14] + "<h3/>")
+}
+
+else if (input === 16) {
+    document.write("<h1>" + roza[15] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[15] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[15] + "<h3/>")
+}
+
+else if (input === 17) {
+    document.write("<h1>" + roza[16] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[16] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[16] + "<h3/>")
+}
+
+else if (input === 18) {
+    document.write("<h1>" + roza[17] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[17] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[17] + "<h3/>")
+}
+
+else if (input === 19) {
+    document.write("<h1>" + roza[18] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[18] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[18] + "<h3/>")
+}
+
+else if (input === 20) {
+    document.write("<h1>" + roza[19] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[19] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[19] + "<h3/>")
+}
+
+else if (input === 21) {
+    document.write("<h1>" + roza[20] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[20] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[20] + "<h3/>")
+}
+
+else if (input === 22) {
+    document.write("<h1>" + roza[21] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[21] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[21] + "<h3/>")
+}
+
+else if (input === 23) {
+    document.write("<h1>" + roza[22] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[22] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[22] + "<h3/>")
+}
+
+else if (input === 24) {
+    document.write("<h1>" + roza[23] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[23] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[23] + "<h3/>")
+}
+
+else if (input === 25) {
+    document.write("<h1>" + roza[24] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[24] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[24] + "<h3/>")
+}
+
+else if (input === 26) {
+    document.write("<h1>" + roza[25] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[25] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[25] + "<h3/>")
+}
+
+else if (input === 27) {
+    document.write("<h1>" + roza[26] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[26] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[26] + "<h3/>")
+}
+
+else if (input === 28) {
+    document.write("<h1>" + roza[27] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[27] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[27] + "<h3/>")
+}
+
+else if (input === 29) {
+    document.write("<h1>" + roza[28] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[28] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[28] + "<h3/>")
+}
+
+else if (input === 30) {
+    document.write("<h1>" + roza[29] + "<h1/>" + "<h3>" + "Sehr Time is " + sehrTime[29] + "<h3/>" + "<h3>" + "Iftar Time is " + iftarTime[29] + "<h3/>")
+}
+
